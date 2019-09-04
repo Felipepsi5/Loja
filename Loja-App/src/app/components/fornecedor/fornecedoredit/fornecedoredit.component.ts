b80a23c0efd507ac43e25aeca884efaa3467e0d1
Felipe Pimenta Silva <fpiments@everis.com>
2019-09-04 15:35:24 -0300

    Incluindo novas funcionalidades

--- Loja-App/src/app/components/fornecedor/fornecedoredit/fornecedoredit.component.ts
+++ Loja-App/src/app/components/fornecedor/fornecedoredit/fornecedoredit.component.ts
@@ -2,6 +2,7 @@ import { ToastrModule, ToastrService } from 'ngx-toastr';
 import { LojaService } from './../../../services/loja.service';
 import { Component, OnInit } from '@angular/core';
 import { Fornecedores } from 'src/app/_models/Fornecedores';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-fornecedoredit',
@@ -10,6 +11,8 @@ import { Fornecedores } from 'src/app/_models/Fornecedores';
 })
 export class FornecedoreditComponent implements OnInit {
   _filtroLista = '';
+  bodyDeletarFornecedor = '';
+  registerForm: FormGroup;
   fornecedoresFiltrados: Fornecedores[];
   fornecedores: Fornecedores[];
   fornecedor: Fornecedores;
@@ -37,15 +40,34 @@ export class FornecedoreditComponent implements OnInit {
   }
   getFornecedores() {
     this.lojaService.getAllFornecedor().subscribe(
-      (_fornecedor: Fornecedores[]) => {
-        this.fornecedores = _fornecedor;
+      (fornecedor: Fornecedores[]) => {
+        this.fornecedores = fornecedor;
         this.fornecedoresFiltrados = this.fornecedores;
-        console.log(this.fornecedores)
+
       }, error => {
         this.toastr.error(`Erro ao tentar carregar Fornecedor`);
 
       }
-    )
+    );
+  }
+  excluirFornecedor(fornecedor: Fornecedores, template: any) {
+    this.openModal(template);
+    this.fornecedor = fornecedor;
+    this.bodyDeletarFornecedor = `Tem certeza que deseja excluir o Fornecedor: ${fornecedor.nomedaempresa}`;
+  }
+  confirmeDelete(template: any) {
+    this.lojaService.deleteFornecedor(this.fornecedor.id).subscribe(
+      () => {
+        this.getFornecedores();
+        this.toastr.success('Deletado com Sucesso');
+      }, error => {
+        this.toastr.error('Erro ao tentar Deletar');
+      }
+    );
+  }
+
+  openModal(template: any) {
+    template.show();
   }
 
 }
